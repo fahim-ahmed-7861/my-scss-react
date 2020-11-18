bfe45490e6a7268ba257b25da94df312a5563ddf
fahim-ahmed-7861 <fahimahmedshojib109@gmail.com>
2020-11-19 00:14:46 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import FriendDetails from './Components/FriendDetails/FriendDetails';
 
 function App() {
 
+  const imgStyle = {
+    height: '200px',
+    width: '200px',
+    textAlign: 'center', 
+    paddingLeft : '250px'
+  }
+
   const [count,setCount]=useState('Mobile');
 
   return (
     
     <CategoryContext.Provider value={[count, setCount]}>
+      <img style={imgStyle} src='https://i.picsum.photos/id/1001/5616/3744.jpg?hmac=38lkvX7tHXmlNbI0HzZbtkJ6_wpWyqvkX4Ty6vYElZE'></img>
     <div className="App">
 
 
